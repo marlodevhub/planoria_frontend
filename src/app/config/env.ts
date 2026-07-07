6f676edad2122f6b1c6d1e5ae09b37b96de69414
angel <angel.nvz301606@gmail.com>
2026-07-07 01:30:35 -0500

    chore: update API URL to PLANORIA.somee.com

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -1,3 +1,3 @@
 export const env = {
-    apiUrl: import.meta.env.VITE_API_URL || 'http://localhost:7075/api',
+    apiUrl: import.meta.env.VITE_API_URL || 'http://PLANORIA.somee.com/api',
 } as const
